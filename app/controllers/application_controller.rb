f700dd597c282ab23519eb31c8e5933bd8f2b31b
Arbind <arbind@carbonfive.com>
2013-07-22 21:54:28 -0700

    responsive design for iphone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
+  rescue_from Exception, :with => :redirect_to_signup_path
 
   def current_user
     @current_user ||= TwitterUser.where(twitter_id: session[:current_user]).first
@@ -16,13 +17,15 @@ class ApplicationController < ActionController::Base
   end
 
   def ensure_login
-    redirect_to signup_path unless current_user
+    redirect_to_signup_path unless current_user
   end
 
+  def redirect_to_signup_path
+    redirect_to signup_path
+  end
 end
 
 class ApplicationController::Protected < ApplicationController
   layout 'application_protected'
   before_action :ensure_login
 end
-
